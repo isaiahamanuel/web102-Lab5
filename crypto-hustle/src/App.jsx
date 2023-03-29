2ba85ca575f8689ad5818c855d6587643e562dd0
Isaiah Amanuel <isaiahamanuel@gmail.com>
2023-03-28 21:48:30 -0700

    components for dashboard

--- crypto-hustle/src/App.jsx
+++ crypto-hustle/src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import CoinInfo from "./Components/coinInfo";
+import SideNav from "./Components/sideNav";
+import "./App.css";
+
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [list, setList] = useState(null);
+  const searchItems = (searchValue) => {
+    setSearchInput(searchValue);
+    if (searchValue !== "") {
+      const filteredData = Object.keys(list.Data).filter((item) =>
+        Object.values(item)
+          .join("")
+          .toLowerCase()
+          .includes(searchValue.toLowerCase())
+      );
+      setFilteredResults(filteredData);
+    } else {
+      setFilteredResults(Object.keys(list.Data));
+    }
+  };
+  const fetchAllCoinData = async () => {
+    const response = await fetch(
+      "https://min-api.cryptocompare.com/data/all/coinlist?&api_key" + API_KEY
+    );
+    const json = await response.json();
+    setList(json);
+  };
+  useEffect(() => {
+    fetchAllCoinData().catch(console.error);
+  }, []);
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="whole-page">
+      <SideNav />
+      <h1>My Crypto List</h1>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchInput}
+        onChange={(inputString) => searchItems(inputString.target.value)}
+      />
+      <ul>
+        {searchInput.length > 0
+          ? filteredResults.map((coin) =>
+              list.Data[coin].PlatformType == "blockchain" ? (
+                <CoinInfo
+                  image={list.Data[coin].ImageUrl}
+                  name={list.Data[coin].FullName}
+                  symbol={list.Data[coin].Symbol}
+                  key={list.Data[coin].Symbol}
+                />
+              ) : null
+            )
+          : list &&
+            Object.entries(list.Data).map(([coin]) =>
+              list.Data[coin].PlatformType == "blockchain" ? (
+                <CoinInfo
+                  image={list.Data[coin].ImageUrl}
+                  name={list.Data[coin].FullName}
+                  symbol={list.Data[coin].Symbol}
+                  key={list.Data[coin].Symbol}
+                />
+              ) : null
+            )}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ crypto-hustle/src/Components/sideNav.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import CryptoScam from "./CryptoScam";
+
+const SideNav = () => (
+  <div className="sidenav">
+    <h1>here is my side nav bar</h1>
+  </div>
+);
+
+export default SideNav;
